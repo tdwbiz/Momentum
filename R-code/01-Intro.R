73455a367d29a74d720aef4ea5bbc503f3ff1949
epwalsh <epwalsh10@gmail.com>
2015-01-20 15:56:01 -0600

    a

--- R-code/01-Intro.R
+++ R-code/01-Intro.R
@@ -1,9 +1,13 @@
 # Introduction
 
 source('~/GitHub/Momentum/R-code/utils.R')
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+
 
 # First two figures in introduction - log cumulative return of small and big momentum stocks
-####################################################################################################
+#############################################################################################
 
 mom <- read.table("~/GitHub/Momentum/Data/25_Portfolios_ME_Prior_12_2.txt", header=TRUE, quote="\"")
 size <- read.table("~/GitHub/Momentum/Data/Portfolios_Formed_on_ME.txt", header=TRUE, quote="\"")
@@ -15,7 +19,6 @@ gsub('^([0-9]{4})([0-9]+)', '\\1-\\2-01', date)
 
 mom$Date <- gsub('^([0-9]{4})([0-9]+)', '\\1-\\2-01', mom$Date)
 
-library(dplyr)
 mom <- mom %>% select(c(1, 6, 26))
 names(mom) <- c("Date", "SH", "BH")
 
@@ -45,8 +48,6 @@ mom <- mom %>% mutate(LogCumLo20 = log(CumLo20),
                       LogCumBH = log(CumBH))
 
 # plot log cumulative returns
-library(ggplot2)
-library(reshape2)
 
 # 01-Lo20
 mom %>% select(c(1,10,12)) %>% melt(id.vars="Date", value.name="CumReturn") %>%
@@ -59,3 +60,18 @@ mom %>% select(c(1,11,13)) %>% melt(id.vars="Date", value.name="CumReturn") %>%
   ggplot(aes(Date, CumReturn)) + 
   geom_line(aes(colour=factor(variable, labels=c("High 20 ME","Momentum")))) +
   labs(colour="Portfolio") + theme_bw() + ylab("Log cumulative return")
+
+# First table: summary of performance of momentum decile portfolios
+#############################################################################################
+
+mom <- read.table("~/GitHub/Momentum/Data/10_Portfolios_Prior_12_2_year.txt", header=TRUE, quote="\"")
+mom$Date <- gsub('^([0-9]{4})([0-9]+)', '\\1-\\2-01', mom$Date)
+names(mom) <- c("Date","Low",as.character(2:9),"High")
+mkt <- read.table("~/GitHub/Momentum/Data/F-F_Research_Data_Factors_year.txt", header=TRUE, quote="\"")
+mkt$Date <- gsub('^([0-9]{4})([0-9]+)', '\\1-\\2-01', mkt$Date)
+
+mom <- inner_join(mkt, mom, by="Date")
+rm(mkt)
+
+regL <- lm(Low~Mkt.RF+SMB+HML, data=mom)
+regH <- lm(High~Mkt.RF+SMB+HML, data=mom)
