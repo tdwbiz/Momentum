05f9f6dc6253d372478baca55ca41bf3058d9241
epwalsh <epwalsh10@gmail.com>
2015-01-21 19:15:44 -0600

    a

--- R-code/03-Betas.R
+++ R-code/03-Betas.R
@@ -6,15 +6,20 @@ library(reshape2)
 library(quantmod)
 source('~/GitHub/Momentum/R-code/utils.R')
 
-mkt <- read.table("~/GitHub/Momentum/Data/F-F_Research_Data_Factors_daily.txt", header=TRUE, quote="\"")
-mkt$Date <- as.Date(as.character(mkt$Date), format="%Y%m%d")
+# mkt <- read.table("~/GitHub/Momentum/Data/F-F_Research_Data_Factors_daily.txt", header=TRUE, quote="\"")
+# mkt$Date <- as.Date(as.character(mkt$Date), format="%Y%m%d")
 
-getSymbols("AAPL",src="yahoo", from = as.Date("2012-01-01"), to = as.Date("2014-12-31")) 
 getSymbols("^GSPC", src = "yahoo", from = as.Date("2012-01-01"), to = as.Date("2014-12-31")) 
+getSymbols("AAPL",src="yahoo", from = as.Date("2012-01-01"), to = as.Date("2014-12-31")) 
+getSymbols("GOOG",src="google", from = as.Date("2012-01-01"), to = as.Date("2014-12-31")) 
 
-AAPL$Close <- AAPL$AAPL.Close
+aapl_betas <- getBetas(AAPL,GSPC$GSPC.Close)
+goog_betas <- getBetas(GOOG, GSPC$GSPC.Close)
 
-x <- getBetas(AAPL,GSPC$GSPC.Close)
-
-x %>% ggplot(aes(Date, betas)) + 
-  geom_line() + theme_bw() 
+aapl_betas %>% inner_join(goog_betas, by="Date") %>%
+  transmute(Date = as.Date(Date, format="%Y-%m-%d"),
+            AAPL = betas.x,
+            GOOG = betas.y) %>%
+  melt(id.vars="Date", value.name="Beta") %>%
+  ggplot(aes(Date, Beta)) + geom_line(aes(colour=variable)) +
+  labs(colour="Company") + theme_bw()
--- R-code/utils.R
+++ R-code/utils.R
@@ -16,13 +16,14 @@ getBetas <- function(df, mkt, k = 126) {
   # with regard to mkt - a vector
   # 10 daily lags are used for mkt
   # uses close prices
+  j <- grep("close", names(df), ignore.case=T)[1]
   n <- nrow(df)
   stopifnot(k > 0 & n == nrow(mkt))
   betas <- rep(NA, n-k-9)
   Date <- index(df)[(k+10):n]
   
   for (i in (k+10):n) {
-    reg <- lm(df$Close[(i-k+1):i]~mkt[(i-k+1):i]+mkt[(i-k):(i-1)]+mkt[(i-k-1):(i-2)]+
+    reg <- lm(df[(i-k+1):i,j]~mkt[(i-k+1):i]+mkt[(i-k):(i-1)]+mkt[(i-k-1):(i-2)]+
                 mkt[(i-k-2):(i-3)]+mkt[(i-k-3):(i-4)]+mkt[(i-k-4):(i-5)]+
                 mkt[(i-k-5):(i-6)]+mkt[(i-k-6):(i-7)]+mkt[(i-k-7):(i-8)]+
                 mkt[(i-k-8):(i-9)]+mkt[(i-k-9):(i-10)])
